dde3a19f2af2803693353d2b8a7c30a747ac2dc4
tangxiangmin <645234650@qq.com>
2017-01-01 10:59:58 +0800

    build directory

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var gulp =  require('gulp');
+var livereload = require('gulp-livereload');
+
+gulp.task('watch',function () {
+   livereload.listen();
+
+   gulp.watch(['./assets/css/*.css','./html/*.html','./*.html'],function (file) {
+       livereload.changed(file.path);
+   })
+});
