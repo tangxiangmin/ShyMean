a31036cae871dda0b7383ac2a5a2f5a2660747d5
tangxiangmin <645234650@qq.com>
2017-08-01 01:08:29 +0800

    visitor

--- server/model/Visitor.js
+++ server/model/Visitor.js
@@ -2,21 +2,35 @@
  * Created by admin on 2017/7/15.
  */
 let Model = require("../core/Model");
+let axios = require("axios");
 
-class Visitor extends Model{
-    constructor(tableName){
-        super();
-        this._tableName = "shymean_visitor";
-        this._primarykey = "id";
-    }
-
-    saveVisitRecord(ip, referrer){
-        return this.add({
-            ip,
-            referrer,
-            created_at: Date.now()/1000
+let Visitor = new Model("shymean_visitor");
+Object.assign(Visitor, {
+    saveVisitRecord(ip, referrer = ""){
+        return this.getIpLocation(ip).then(res=>{
+            let { country="", province="", city="", district="" } = res;
+            return this.insert({
+                ip,
+                referrer,
+                country,
+                province,
+                city,
+                district,
+                created_at: Date.now()/1000
+            })
+        })
+        // this.insert({
+        //     ip,
+        //     referrer,
+        //     created_at: Date.now()/1000
+        // });
+    },
+    getIpLocation(ip){
+        let url = `http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=${ip}`;
+        return axios.get(url).then(res=>{
+            return res.data;
         });
     }
-}
+});
 
-module.exports = new Visitor;
+module.exports = Visitor;
