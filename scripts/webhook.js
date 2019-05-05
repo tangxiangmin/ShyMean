534a6a1852e9e466f55f3d747e15c34963c23f49
tangxiangmin <645234650@qq.com>
2019-05-05 23:40:59 +0800

    test auto deploy

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -31,9 +31,9 @@ handler.on('push', function (event) {
     });
 });
 
-function rumCommand( cmd, args, callback ){
+function runCommand( cmd, args, callback ){
     let child = spawn( cmd, args );
     let response = '';
-    child.stdout.on('data', function( buffer ){ resp += buffer.toString(); });
-    child.stdout.on('end', function(){ callback( resp ) });
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); });
+    child.stdout.on('end', function(){ callback( response ) });
 }
