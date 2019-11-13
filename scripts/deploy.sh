778ddf6c738a630f6e099bab411ca29dfbb48434
tangxiangmin <645234650@qq.com>
2019-11-13 23:00:52 +0800

    fix: 修改deploy.sh更新步骤

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,9 @@
 
 git pull origin master
 npm i
-npm run build
+# npm run build:server
+npm run build:ssr
 
-# blog任务名称在package.json中start命令设置
+# pm2任务名称在package.json中start命令设置
 pm2 reload blog
 pm2 reload blog_ssr
